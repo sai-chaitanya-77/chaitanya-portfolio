43b77b0147f0cc5c44cb24323dd2da862a2506c8
sai-chaitanya-77 <saichaitanyadhulipalla77@gmail.com>
2026-02-08 00:25:24 +0530

    Added mobile navbar

--- chaitanya-portfolio/src/components/Navbar.tsx
+++ chaitanya-portfolio/src/components/Navbar.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
 const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+
     return (
         <header className="fixed top-0 left-0 w-full z-50 bg-gray-950/80 backdrop-blur border-b border-white/10">
             <nav className="max-w-6xl mx-auto px-4 h-16 flex items-center justify-between">
                 <span className="text-xl font-bold tracking-wide">
                     Sai<span className="text-indigo-400">.</span>
                 </span>
+
+                {/* Desktop navbar */}
                 <ul className="hidden md:flex gap-8 text-sm text-gray-300">
                     <li>
                         <a href="#about" className="hover:text-white cursor-pointer">About</a>
@@ -19,7 +25,41 @@ const Navbar = () => {
                         <a href="#contact" className="hover:text-white cursor-pointer">Contact</a>
                     </li>
                 </ul>
+
+                {/* Mobile hamburger menu */}
+                <button
+                    className="md:hidden text-gray-300"
+                    onClick={() => {
+                        setNavbarOpen(!navbarOpen)
+                    }}
+                >
+                    ☰
+                </button>
             </nav>
+
+            {/* Mobile Nav menu */}
+            {navbarOpen && (
+                <div className="md:hidden bg-gray-950 border-t border-white/10">
+                    <div className="flex flex-col px-4 py-4 gap-4 text-gray-300">
+                        {
+                            ["about", "skills", "projects", "contact"].map((item) => {
+                                return (
+                                    <a
+                                        key={item}
+                                        href={`#${item}`}
+                                        className="hover:text-white"
+                                        onClick={() => {
+                                            setNavbarOpen(false)
+                                        }}
+                                    >
+                                        {item.charAt(0).toUpperCase() + item.slice(1)}
+                                    </a>
+                                );
+                            })
+                        }
+                    </div>
+                </div>
+            )}
         </header>
     );
 };
